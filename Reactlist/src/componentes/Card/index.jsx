6ed26623e50b6b3432c5302851cf90e5ef2de215
Samuel Araujo <samuel.araujo.carvalho@gmail.com>
2022-07-11 18:14:51 -0300

    Adicionando _Componentes

---
+++ Reactlist/src/componentes/Card/index.jsx
@@ -0,0 +1,10 @@
+import './styles.css';
+
+export function Card () {
+    return(
+        <div className='card'>
+            <strong> Samuel Araujo</strong>
+            <small> 17:14:24</small>
+        </div>
+    )
+}
--- Reactlist/src/pages/Home/index.jsx
+++ Reactlist/src/pages/Home/index.jsx
@@ -1,11 +1,17 @@
 import './styles.css';
 
+import{ Card } from '../../componentes/Card';
+
 export function Home() {
   return (
-    <div>
+    <div className='container'>
       <h1>Lista de presença</h1>
       <input type="text" placeholder="Digite o nome" />
       <button type="button">Adicionar</button>
+      
+      <Card/>
+      <Card/>
+      <Card/>
     </div>
   )
    
