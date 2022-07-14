73fbe1832bfe89d7501c55652e90d5cbaa8aeee8
Samuel Araujo <samuel.araujo.carvalho@gmail.com>
2022-07-14 13:29:06 -0300

    Finalizado_Projeto

--- Reactlist/src/pages/Home/index.jsx
+++ Reactlist/src/pages/Home/index.jsx
@@ -1,29 +1,71 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import './styles.css';
 import{ Card } from '../../componentes/Card';
 
+//estado, armazenando conteudo atual digitado do Input
 export function Home() {
   const [studentName, setStudentName] = useState('');
+  const [students, setstudents] = useState([]);
+  const [ user, setUser] = useState({name:'', avatar: ''});
+
+  /*quando esta função for chamada ela vai criar um novo objetos com duas propriedades, (Nome que vai esta armazenado no estado atual é hora atual do sistema)
+  */
+  function handleAddStudent() {
+    const newStudent = {
+      name: studentName,
+      time: new Date().toLocaleTimeString("pt-br",  {
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+      })
+    }
+
+    // Utilizando spread operator (...) para pegar oconteudo do estado anterior e colocar dentro do vetor/Array e mostrar na interface. princpio da imutabilidade
+    setstudents(sprevState => [...sprevState, newStudent]);
+  }
+
+
+  //utilizando  API do Github para pegar de forma dinâmica nome e avatar e colocar na interface
+  useEffect(() => {
+   fetch('https://api.github.com/users/samuelsam8')
+   .then(response => response.json())
+   .then(data =>{
+    setUser({
+      name: data.name,
+      avatar: data.avatar_url,
+    })
+   })
+  },[]);
 
   return (
-    <div className='container'>
-      <h1>Nome: {studentName} </h1>
+    <div className="container">
+      <header>
+        <h1>Lista de presença</h1>
+        <div>
+        <strong>{user.name}</strong>
+        <img src={user.avatar} alt="Foto do perfil GitHub"/>
+        </div>
+      </header>
 
       <input type="text" 
-      placeholder="Digite o nome" 
+      placeholder="Digite o nome..." 
       onChange={ e => setStudentName(e.target.value)}
       />
-     
 
-      <button type="button">Adicionar</button>
-      
-      <Card name="samuel" time="14:00:00"/>
-      <Card name="Luana" time="16:00:00"/>
-      <Card name="Copelia" time="15:00:00"/>
-      
+    <button type="button" onClick={handleAddStudent}>
+      Adicionar
+    </button> 
     
+    {/* deixando cartões dinâmicos. Utilizando Time como Key  já que os segundo vão ser unicos para deixar o componente unico*/
+      students.map(student => (
+      <Card 
+        key={student.time}
+        name={student.name} 
+        time={student.time}
+      />
+    ))
+    }
     </div>
   )
-   
 }
 
